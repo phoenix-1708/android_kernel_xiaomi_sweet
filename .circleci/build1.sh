0bbe8a9d042b91d98fcf83ce57e4885f61400d22
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-05 21:43:52 +0530

    Update build1.sh

--- .circleci/build1.sh
+++ .circleci/build1.sh
@@ -24,17 +24,16 @@ cd android_kernel_xiaomi_sweet
 # git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
 # git clone --depth=1 https://github.com/Haseo97/Avalon-Clang-12.0.0.git clang
 # git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
-# git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-git clone --depth=1 https://github.com/theradcolor/aarch64-linux-gnu aarch64-linux-android-4.9
-git clone --depth=1 https://github.com/theradcolor/arm-linux-gnueabi arm-linux-androideabi-4.9
+# git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+git clone --depth=1 https://github.com/theradcolor/clang aarch64-linux-android
 git clone https://github.com/fabianonline/telegram.sh.git  -b master
 KERNEL_DIR=/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet
 REPACK_DIR="${KERNEL_DIR}/AnyKernel"
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
 SEND_DIR="${KERNEL_DIR}/telegram.sh"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-export PATH="/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/clang/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/aarch64-linux-android-4.9/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/arm-linux-androideabi-4.9/bin:$PATH"
+export PATH="/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/clang/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/aarch64-linux-android/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/aarch-linux-android/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_USER=phoenix-1708
 export KBUILD_BUILD_HOST=ubuntu
