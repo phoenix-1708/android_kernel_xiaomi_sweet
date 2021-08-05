e13c4adc4c8cca1832d208bbc13d03be066bafee
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-05 20:47:48 +0530

    Update build1.sh

--- .circleci/build1.sh
+++ .circleci/build1.sh
@@ -24,19 +24,17 @@ cd android_kernel_xiaomi_sweet
 # git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
 # git clone --depth=1 https://github.com/Haseo97/Avalon-Clang-12.0.0.git clang
 # git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
-# git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-git clone https://github.com/stormbreaker-project/aarch64-linux-android-4.9.git
-git clone https://github.com/stormbreaker-project/arm-linux-androideabi-4.9.git
+# git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+git clone https://github.com/stormbreaker-project/aarch64-linux-android-4.9.git aarch64-linux-android-4.9
+git clone https://github.com/stormbreaker-project/arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
 git clone https://github.com/fabianonline/telegram.sh.git  -b master
 KERNEL_DIR=/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet
 REPACK_DIR="${KERNEL_DIR}/AnyKernel"
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
-DTB_T="${KERNEL_DIR}/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-tissot-treble.dtb"
-DTB="${KERNEL_DIR}/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-tissot-nontreble.dtb"
 SEND_DIR="${KERNEL_DIR}/telegram.sh"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-export PATH="/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/linux-x86/clang-r416183c/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/aarch64-linux-android-4.9/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/arm-linux-androideabi-4.9/bin:$PATH"
+export PATH="/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/clang/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/aarch64-linux-android-4.9/bin:/home/runner/work/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/arm-linux-androideabi-4.9/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_USER=phoenix-1708
 export KBUILD_BUILD_HOST=ubuntu
