0cd1961c193fd29086241af1307a6d123f98ec00
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-06 08:16:18 +0530

    Update zip.sh

--- .circleci/zip.sh
+++ .circleci/zip.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+git clone https://github.com/fabianonline/telegram.sh.git  -b master
+
 # Zipping
 KERNEL_DIR=$(pwd)
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
-cd telegram.sh
+SEND_DIR="${KERNEL_DIR}/telegram.sh"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+cd $SEND_DIR
 ./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -509071822 -f $IMAGE
 echo "Zip Sent through GithubActions"
    #curl --upload-file ./PhoenixKernel_NonOC.zip https://transfer.sh/PhoenixKernel_NonOC.zip
