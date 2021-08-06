24f6efeb51c8b0207606857ffeb021402b5fdc59
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-06 08:16:22 +0530

    Update build1.sh

--- .circleci/build1.sh
+++ .circleci/build1.sh
@@ -24,16 +24,17 @@ cd android_kernel_xiaomi_sweet
 # git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
 # git clone --depth=1 https://github.com/Haseo97/Avalon-Clang-12.0.0.git clang
 # git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
-# git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-git clone --depth=1 https://github.com/theradcolor/clang.git aarch64-linux-android
-git clone https://github.com/fabianonline/telegram.sh.git  -b master
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 linux-x86
+# git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+git clone https://github.com/stormbreaker-project/aarch64-linux-android-4.9.git aarch64-linux-android-4.9
+git clone https://github.com/stormbreaker-project/arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
+# git clone https://github.com/fabianonline/telegram.sh.git  -b master
 KERNEL_DIR=$(pwd)
 REPACK_DIR="${KERNEL_DIR}/AnyKernel"
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
-SEND_DIR="${KERNEL_DIR}/telegram.sh"
+# SEND_DIR="${KERNEL_DIR}/telegram.sh"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-export PATH="$(pwd)/clang/bin:$(pwd)/aarch64-linux-android/bin:$(pwd)/aarch-linux-android/bin:$PATH"
+export PATH="$(pwd)/linux-x86/clang-r416183c/bin:$(pwd)/aarch64-linux-android-4.9/bin:$(pwd)/arm-linux-androideabi-4.9/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_USER=phoenix-1708
 export KBUILD_BUILD_HOST=ubuntu
@@ -47,6 +48,6 @@ make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
                           
